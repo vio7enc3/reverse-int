6215617d3702c7fae3268bb71643079f0bdd93bd
vio7enc3 <70806255+vio7enc3@users.noreply.github.com>
2020-10-12 20:33:59 +0500

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    var pos = (n ** 2) ** 0.5;
+    var arr = pos.toString().split('');
+    var arrRev = +arr.reverse().join('');
+    return arrRev;
 }
